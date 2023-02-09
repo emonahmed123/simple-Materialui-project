1581acfc53a5d66438ec9be8d5c6bf3c4eeb96c8
emonahmed123 <imonshomon@gmail.com>
2023-02-09 21:37:59 +0600

    added material-ul

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <Button color='secondary' style={{backgroundColor
+      :"blue",color:"red"}}  size='large' variant='contained'>Click me</Button>
     </div>
   );
 }
